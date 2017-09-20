ac7605c9389d1255cdf067bacc42b261f7ceabac
rne23 <rne23@cornell.edu>
2017-09-19 20:12:54 -0700

    index and script
    index and script

---
+++ js/script.js
@@ -0,0 +1,7 @@
+var message = "Hey Ryeker! I am taking this class because I enjoy learning and"+
+" I'm interested in becoming a web developer.  My favorite class at university"+
+" was Intro to Python.  I am currently filling gaps in my HTML/CSS knowledge,"+
+" as well as responsive design.  I know a bit about loops, if/else statements,"+
+" and functions, and would love to help others understand these concepts."
+
+console.log(message)
